bfc11315a4b98639722e49e63f62c21ee851b802
unknown <barclar@HOU-W7722301191.conoco.net>
2014-05-24 07:22:37 -0500

    Code for prog. assmnt. 2 Initial version

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 
 }
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()  #call the getinv method
+  if (!is.null(m)) {   #if the retrun is not null we have the inv.  matrix
+    message("getting cached inverse")
+    return(m)  #return the cached value
+  }
+  data <- x$get()
+  m <- solve(data)
+    x$setinv(m)
+    m
+  
 }
